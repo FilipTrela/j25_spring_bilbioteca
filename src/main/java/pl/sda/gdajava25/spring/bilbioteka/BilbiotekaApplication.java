a977d57c28f2959c9bc5258acf4bb342a3c64ed7
Filip Trela <ftrela@gmail.com>
2019-10-02 18:34:40 +0200

    initial commit

---
+++ src/main/java/pl/sda/gdajava25/spring/bilbioteka/BilbiotekaApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.gdajava25.spring.bilbioteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbiotekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilbiotekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/gdajava25/spring/bilbioteka/BilbiotekaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.gdajava25.spring.bilbioteka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BilbiotekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
